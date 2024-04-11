b840bfaf549ff56e91abe0e7e5040ab652adaa02
geloyola <geloyola@utpl.edu.ec>
2024-04-11 17:38:53 -0500

    clase 2 de la semana 1

--- Demo/src/paquete2/Demo002.java
+++ Demo/src/paquete2/Demo002.java
@@ -6,22 +6,35 @@
 package paquete2;
 import paquete1.Mensaje;
 import paquete3.Informacion;
+import java.util.Scanner;
 
 
 /**
  *
  * @author reroes
  */
-public class Demo {
+public class Demo002 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner leer= new Scanner(System.in);
         String cadena1= Mensaje.obtenerMensaje(); 
         int habitantes= Informacion.obtenerHabitantes();
+        int opcion;
+        String cadenaFinal= String.format("%s\n con %d\n", cadena1, habitantes);
+        opcion= leer.nextInt();
+       
+        if (opcion<=5 && opcion>=1){
+            System.out.printf("%s\n;", cadenaFinal);
+        }
+        else{
+            System.out.printf("%s\n", cadenaFinal.toUpperCase() );
+            
+        }
         // ingresar la ruta del paquete1 y finalizar con parentesis
-        System.out.printf("%s\n con %d\n", cadena1, habitantes);
+        
         // usar printf;
     }
     
